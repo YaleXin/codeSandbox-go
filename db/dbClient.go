4e0d7dc3afa7cfbba01b9a1336b19d7771714565
YaleXin <495592364@qq.com>
2024-06-22 22:16:17 +0800

    优化数据库客户端初始化方式

--- db/dbClient.go
+++ db/dbClient.go
@@ -1,25 +1,56 @@
 package db
 
 import (
+	"codeSandbox/model"
+	"codeSandbox/utils"
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"time"
 )
 
 var dBClinet *gorm.DB
 
 func init() {
 	// 初始化客户端
-	dsn := "root:123456@tcp(localhost:3307)/code_sandbox_data?charset=utf8mb4&parseTime=True&loc=Local"
+
+	log.Info("init database...")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		utils.Config.Server.Database.User, utils.Config.Server.Database.PassWord, utils.Config.Server.Database.Host, utils.Config.Server.Database.Port, utils.Config.Server.Database.Name)
 	var err error
 	dBClinet, err = gorm.Open(mysql.New(mysql.Config{
-		DSN: dsn,
-	}))
+		DSN:                       dsn,
+		DefaultStringSize:         256,  // string 类型字段的默认长度
+		DisableDatetimePrecision:  true, // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true, // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true, // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: true, // 根据当前 MySQL 版本自动配置
+	}), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
 	if err != nil {
-
+		log.Panic(fmt.Sprintf("database connect fail,%s", err))
 	}
-	log.Infof("******* db init %v ******* ", dBClinet)
-
-	// 初始化表
+	sqlDB, err := dBClinet.DB()
+	if err != nil {
+		log.Panic(fmt.Sprintf("database init config fail,%s", err))
+	}
+	// TODO 添加所有的结构体
+	err = dBClinet.AutoMigrate(&model.User{})
+	if err != nil {
+		log.Panic(fmt.Sprintf("database migrate fail,%s", err))
+	}
+	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxIdleConns(10)
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(100)
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(10 * time.Second)
+	//Db.Create(&User{Username: "root", Password: "123456", Role: 0})
+	log.Info("init database success!")
 
 }
--- model/user.go
+++ model/user.go
@@ -1,11 +1,13 @@
 package model
 
+import "gorm.io/gorm"
+
 type User struct {
-	Model
-	Username string `gorm:"type:varchar(40);not null;comment:用户名" json:"username"` //用户名
-	Password string `gorm:"type:varchar(100);not null;comment:密码" json:"password"` //密码
-	Salt     string `gorm:"type:varchar(20);not null;comment:加密盐" json:"salt"`     //加密盐
-	Role     int    `gorm:"type:int;comment:权限" json:"role"`                       //权限 0管理员 1 普通用户
+	gorm.Model
+	Username string `gorm:"index;type:varchar(40);not null;comment:用户名" json:"username"` //用户名
+	Password string `gorm:"type:varchar(100);not null;comment:密码" json:"password"`       //密码
+	Salt     string `gorm:"type:varchar(20);not null;comment:加密盐" json:"salt"`           //加密盐
+	Role     int    `gorm:"type:int;comment:权限" json:"role"`                             //权限 0管理员 1 普通用户
 }
 
 func (u User) TableName() string {
